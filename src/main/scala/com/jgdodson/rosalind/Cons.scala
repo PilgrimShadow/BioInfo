cf00aaa7f9264c09790b83d0bd4978739093e2ca
Jordan Dodson <jordandodson22@gmail.com>
2017-05-03 16:10:38 -0500

    updating .travis.yml

--- src/main/scala/com/jgdodson/rosalind/Cons.scala
+++ src/main/scala/com/jgdodson/rosalind/Cons.scala
@@ -6,8 +6,10 @@ object Cons {
 
   def main(args: Array[String]): Unit = {
     val pairs = Utils.readFastaFile(args(0))
+
     val strings = pairs.map(_._2).map(DNAString(_))
     val profile = DNAString.profileMatrix(strings)
+
     println(DNAString.consensusFromProfile(profile))
     println(formatProfile(profile))
   }
